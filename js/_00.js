009d3a76257e6c45b42f97d332b8182c0e4946c8
ClaudeLanthier <claude.lanthier@gmail.com>
2015-10-26 11:27:46 -0400

    Commit 2

--- js/_00.js
+++ js/_00.js
@@ -1,7 +1,15 @@
-﻿(function(){
+﻿function cl(v){console.log(v)};
+(function(){
 var $_fob={
 	ini:function(){
-		$fob.dom.layout()
+		$fob.dom.layout();
+		$fob.dom.menu.forms();
+		$fob.dom.menu.canvas();
+		$fob.dom.menu.controls();
+		$fob.xxx.ini_size();
+		
+
+
 	}
 }
 this.$_fob=$_fob;
--- js/fob.js
+++ js/fob.js
@@ -4,28 +4,115 @@ var $={
     dom:{
         layout:function(){
             no.targs=_o;
-            no.$(document.body,
+            no.$(document.body,{_s:'w1,h1, overflow:hidden;'},
                 no.div(
-                    {id:"con_pag",_s:'rel,bot.0,rg.0,b.c'},
-                    /*no.div({id:"con_top",_s:'abs,b.f,l.0,rg.10,t.10,o.ccc,h.60'},
-                        no.img({src:"img/logo.png",_s:"pl.10,h1,fll,poi"}),
-                        no.div("MAPIT - CRT",{_s:"fll,f.40,ml.15,line-height:59px,ff.Cinzel"},function(){})
+                    {id:"con_pag",_s:'rel,bot.0,rg.0,b.1,w1,h1,overflow:hidden;'},
+                    no.div({id:"con_lft_top",_s:'abs,w.239,h.59,l.10,t.10,b.0,o.222'},
+                        no.img({src:"img/logo.png",_s:"abs,l.15,t.10,h.40"}),
+                        no.div({id:"con_lft",_s:'abs,l.68,t.18,f.20,c.f'},"FORM BUILDER")
                     ),
-                    no.div({id:"con_lft_top",_s:'abs,w.23%,t.80,h.127,b.f,o.ccc,fll,ova'}),*/
-                  //  no.div({id:"con_lft",_s:'abs,t.0,bot.10,l.0,rg.178,b.c'}),
-                  //  no.div({id:"con_lft",_s:'abs,t.0,bot.10,l.0,rg.178,b.c'}),
-                    no.div({id:"con_lft",_s:'abs,h.900,w.100,b.c'}),
-                    no.div({id:"con_cen",_s:'abs,h.900,w.1600,rg.0,b.3'})
-
-                ),
-                no.div({id:"con_temp",_s:'w1'})
+                    no.div({id:"con_lft",_s:'abs,w.240,l.10,t.81,bot.10'}),
+                    no.div({id:"con_cen",_s:'abs,l.260,bot.10,rg.10,t.10,p.10,box,o.2,b.0,overflow:auto'},
+                        no.div({id:"canvas",_s:'cen,h.800,w.1200,box,o.3,background:url("img/light.png")repeat; overflow:auto;margin:0 auto'})
+                    )
+                )
             );
             no.targs=false;
         },
+        menu:{
+            forms:function(){
+                no.div(_o.con_lft,{id:"forms_mnu",_c:"mnu_ssec"},
+                    no.div({id:"canv_ttl",_c:"sec_ttl"}, "FORMS"),
+                    no.div(
+                        no.div({_s:"f.14"}, "Study"),
+                        no.input({id:"forms_pr",type:"text"},function onchange(o){})
+                    ),
+                    no.div(
+                        no.div({_s:"f.14"}, "Form name"),
+                        no.input({id:"forms_nm",type:"text"},function onchange(o){})
+                    )
+                )
+            },
+            canvas:function(){
+                no.div(_o.con_lft,{id:"canv_mnu",_c:"mnu_ssec"},
+                    no.div({id:"canv_ttl",_c:"sec_ttl"}, "CANVAS"),
+                    no.div(
+                        no.div({_s:"f.14,dib,w.60"}, "Width"),
+                        no.input({id:"canv_w",type:"number"},function onchange(o){_o.canvas.style.width=o.value+"px";})
+                    ),
+                    no.div(
+                        no.div({_s:"f.14,dib,w.60"}, "Height"),
+                        no.input({id:"canv_h",type:"number"},function onchange(o){_o.canvas.style.height=o.value+"px";})
+                    ),
+                    no.div(
+                        no.div({_s:"f.14,dib,w.60"}, "Color"),
+                        no.input({id:"canv_c",type:"color"},function onchange(o){_o.con_cen.style.background=o.value;})
+                    )
+                )
+            },
+            controls:function(){
+                no.div(_o.con_lft,{id:"ctrl_mnu",_c:"mnu_ssec"},
+                    no.div({id:"ctrl_ttl",_c:"sec_ttl"}, "CONTROLS"),
+                    no.div(
+                        no.img({src:"img/menu.png",_s:"h.20,dib,,m.1|5|1|0"}),
+                        no.div({_s:"f.14,dib"}, "Menu")
+                    ),
+                    no.div(
+                        no.img({src:"img/label.png",_s:"h.20,dib,m.1|5|1|0"}),
+                        no.div({_s:"f.14,dib"}, "Label")
+                    ),
+                    no.div(
+                        no.img({src:"img/radio.png",_s:"h.20,dib,,m.1|5|1|0"}),
+                        no.div({_s:"f.14,dib"}, "Radio")
+                    ),
+                    no.div(
+                        no.img({src:"img/textbox.png",_s:"h.20,dib,,m.1|5|1|0"}),
+                        no.div({_s:"f.14,dib"}, "Textbox")
+                    ),
+                    no.div(
+                        no.img({src:"img/text.png",_s:"h.20,dib,,m.1|5|1|0"}),
+                        no.div({_s:"f.14,dib"}, "Text")
+                    ),
+                    no.div(
+                        no.img({src:"img/checkbox.png",_s:"h.20,dib,,m.1|5|1|0"}),
+                        no.div({_s:"f.14,dib"}, "Checkbox")
+                    ),
+                    no.div(
+                        no.img({src:"img/image.png",_s:"h.20,dib,,m.1|5|1|0"}),
+                        no.div({_s:"f.14,dib"}, "Image")
+                    ),
+                    no.div(
+                        no.img({src:"img/number.png",_s:"h.20,dib,,m.1|5|1|0"}),
+                        no.div({_s:"f.14,dib"}, "Number")
+                    ),
+                    no.div(
+                        no.img({src:"img/date.png",_s:"h.20,dib,,m.1|5|1|0"}),
+                        no.div({_s:"f.14,dib"}, "Date")
+                    )
+                )
+            }
+        },
+        canvas:function(){
+        }
     },
-    frm:{
-        clear:function(){
-
+    xxx:{
+        ini_size:function(){
+           var h=Math.floor(_o.con_cen.clientHeight/10);
+           var w=Math.floor(_o.con_cen.clientWidth/10);
+           _o.canvas.style.height=(h*10-20)+"px";
+           _o.canvas.style.width=(w*10-20)+"px";
+        },
+        new_width:function(o){
+             _o.canvas.style.width=o+"px";
+        }
+    },
+    U:{
+        hex2rgb:function(h){
+            var hex=(h.charAt(0)=="#") ? h.substring(1,7):h,
+                r= parseInt(hex.substring(0,2),16),
+                g= parseInt(hex.substring(2,4),16),
+                b= parseInt(hex.substring(4,6),16);
+            return [r,g,b];
         },
     }
 }
